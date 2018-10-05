7ac9fa7cb773d3ae7df8d18e027466f6262d8a7e
Waskito <waskito.kitoo@gmail.com>
2018-10-06 03:14:23 +0700

    UI Menu and Emergency

---
+++ app/src/main/java/com/imba/kelompol/panicbutton/EmergencyCall.java
@@ -0,0 +1,13 @@
+package com.imba.kelompol.panicbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmergencyCall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_call);
+    }
+}
